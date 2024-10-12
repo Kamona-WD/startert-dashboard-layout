3f996cfdfbd9480c58402236eca4b4ccc65cd4c9
Kamona-WD <ak3849909@gmail.com>
2024-10-13 01:21:29 +0300

    wip

--- src/ts/dev.ts
+++ src/ts/dev.ts
@@ -1,11 +1,43 @@
-import type { Alpine } from 'alpinejs'
+import Alpine from 'alpinejs'
+import persist from '@alpinejs/persist'
+import anchor from '@alpinejs/anchor'
+import resize from '@alpinejs/resize'
 
-export default (Alpine: Alpine) => {
+export default () => {
     document.addEventListener('alpine:init', () => {
-        Alpine.data('setup', function () {
+        Alpine.data('mainState', function () {
             return {
+                init() {
+                    this.$refs.loading?.classList?.add('hidden')
+                },
                 loading: true,
+                isSidebarOpen: false,
+                toggleSidbarMenu() {
+                    this.isSidebarOpen = !this.isSidebarOpen
+                },
+                handleWindowResize() {
+                    if (window.innerWidth <= 1024) {
+                        this.isSidebarOpen = false
+                        //   console.log('1204');
+                    } else {
+                        this.isSidebarOpen = true
+                        // console.log('1');
+                    }
+                },
+                isSearchBoxOpen: false,
             }
+        })
+
+        Alpine.plugin(persist)
+        Alpine.plugin(anchor)
+        Alpine.plugin(resize)
+
+        Alpine.store('darkMode', {
+            on: Alpine.$persist(false),
+
+            toggle() {
+                this.on = !this.on
+            },
         })
     })
 }
